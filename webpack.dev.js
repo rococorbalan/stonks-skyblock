b064cd78b929bdfac54154dfc80471b8761b09f8
rococorbalan <corbaroco@gmail.com>
2025-09-01 01:58:43 -0300

    Change config so webpack starts on incognito tab.

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,5 +6,15 @@ module.exports = merge(common, {
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
+        static: './dist',
+        hot: true,
+        port: 3000,
+        open: {
+            app: {
+                name: 'google-chrome',  
+                arguments: ['--incognito', '--new-window', '--disable-extensions']
+            } 
+        }
     }
+    
 });
